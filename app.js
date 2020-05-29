87a8d32b29f1e189ffa231fc254bdd0023c3f063
Anton Makhnachev <makhnachev.anton@yandex.ru>
2020-05-29 20:58:37 +0300

    configured app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { PORT, SERVER_CONNECT } = require('./config');
+
+const routes = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+mongoose.connect(SERVER_CONNECT, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(requestLogger);
+
+app.use(routes);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+app.use(errors());
+
+app.use(errorLogger);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({ message: statusCode === 500 ? 'Что-то пошло не так' : message });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Port ${PORT}`);
+});
