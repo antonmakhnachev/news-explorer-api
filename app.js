5929b82299f1a6a8f4a21809cb79a5a824406e4c
Anton Makhnachev <makhnachev.anton@yandex.ru>
2020-06-04 22:31:21 +0300

    fixed bugs

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({ message: statusCode === 500 ? 'Что-то пошло не так' : message });
+  next();
 });
 
 
